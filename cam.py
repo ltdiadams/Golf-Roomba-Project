05ddc499ace3b34f9c6f7c6d37f9c4c405fd8c64
Logan DiAdams <ltdiadams@mta.ca>
2020-08-08 02:13:29 -0300

    added comments to detection code

--- cam.py
+++ cam.py
@@ -1,4 +1,4 @@
-# Track white objects with webcam
+# Tracking white golf balls with my webcam
 
 import cv2
 import face_recognition
@@ -6,8 +6,6 @@ import numpy as np
 
 cap = cv2.VideoCapture(0)
 cv2.namedWindow("image")
-# img = cv2.resize(vid, (320, 220))
-# face_locations = []
 
 if cap.isOpened():
     ret, img = cap.read()
@@ -16,41 +14,32 @@ else:
 
 while ret:
 
-    # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
-    # rgb_frame = img[:, :, ::-1]
-
     # cv2.imshow("image", img)
     ret, img = cap.read()
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-    # face_locations = face_recognition.face_locations(rgb_frame)
     lower_white = np.array([0,0,213], dtype=np.uint8)
     upper_white = np.array([172,111,255], dtype=np.uint8)
 
-    # Threshold the HSV image to get only white colors
+    # threshold the HSV image to get only white color
     mask = cv2.inRange(hsv, lower_white, upper_white)
-    # Bitwise-AND mask and original image
+    # residual pixels matching the color mask...
     res = cv2.bitwise_and(img,img, mask= mask)
-
+    
+    # find contours for box tracking
     contours, heirarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     cont_sorted = sorted(contours, key=cv2.contourArea, reverse=True)[:5]
     x,y,w,h = cv2.boundingRect(cont_sorted[0])
     cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),5)
-
-    ####### Facial rec stuff #######
-    # for top, right, bottom, left in face_locations:
-    #
-    #     # Draw a box around the face
-    #     cv2.rectangle(img, (left, top), (right, bottom), (0, 0, 255), 2)
-
-    cv2.imshow('image',img)
-    cv2.imshow('mask',mask)
-    cv2.imshow('res',res)
+    
+    # three windows, og image with tracking, residual pixels, grayscale
+    cv2.imshow('image',img) # og
+    cv2.imshow('mask',mask) # grayscale
+    cv2.imshow('res',res)   # residual
 
     key = cv2.waitKey(20)
-    if key == 27: # exit on ESC
+    if key == 27: # close on ESC
         break
 
-# cv2.destroyWindow("image")
 cv2.destroyAllWindows()
 cap.release()
